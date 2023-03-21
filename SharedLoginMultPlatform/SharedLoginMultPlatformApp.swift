a867a02b4bcc0fe9a5c2cc76e404e420788b1c50
hexch <hexch.c@gmail.com>
2023-03-21 23:42:37 +0900

    Initial Commit

---
+++ SharedLoginMultPlatform/SharedLoginMultPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedLoginMultPlatformApp.swift
+//  SharedLoginMultPlatform
+//
+//  Created by XIAOCHUAN HE on R 5/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SharedLoginMultPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
